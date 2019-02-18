e98cc7318717b89772407f5def779bb9d438d945
Nikita <lepesevich.nikita@yandex.ru>
2019-02-18 09:34:21 +0300

    remove debug in task5.cpp

--- src/task5/task5.cpp
+++ src/task5/task5.cpp
@@ -3,8 +3,6 @@
 bool Task5::IsThePointInArea() {
   const Point first_point(-1.0, 0.0, 1.0);
   const Point second_point(1.0, 0.0, 1.0);
-  cout << "Length to first point: " << point_for_checking_.LengthToOtherPoint(first_point) << endl;
-  cout << "Length to second point: " << point_for_checking_.LengthToOtherPoint(second_point) << endl;
   bool result = false;
   result = result || point_for_checking_.y < 0;
   result = result && (point_for_checking_.IsPointInRadiusOfOtherPoint(first_point) ||
